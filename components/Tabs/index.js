ada961672e0a7096d1051680a9cb9ae6fc126be5
bquizza <blaine555@gmail.com>
2019-07-12 09:35:50 -0600

    tabs index.js finished

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(data => {
+        // console.log(data.data)
+        data.data.topics.forEach(tab => {
+            createTab(tab)    
+        });
+    })
+    .catch(error => {
+        console.log(`error: ${error}`)
+    })
+
+let topics = document. querySelector('.topics')
+
+    createTab = function(topic){
+        tab = document.createElement('div')
+
+        tab.classList.add('tab')
+
+        tab.textContent = topic
+
+        topics.appendChild(tab)
+        // console.log(topics)
+    }
